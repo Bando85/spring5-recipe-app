7042c924c3ab9368577cf021d8679d7194120fc6
Bando85 <laczoandras@gmail.com>
2020-04-05 15:24:49 +0200

    render image from database method added

--- src/test/java/guru/springframework/services/ImageServiceImplTest.java
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.controllers.ImageController;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import org.junit.Before;
@@ -7,7 +9,10 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -16,6 +21,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by Andras Laczo 2020. 04. 05.
@@ -26,12 +33,21 @@ public class ImageServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
+    @Mock
+    RecipeService recipeService;
+
     ImageService imageService;
+
+    MockMvc mockMvc;
+
+    ImageController controller;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
+        controller = new ImageController(imageService, recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
         imageService = new ImageServiceImpl(recipeRepository);
     }
 
@@ -58,6 +74,39 @@ public class ImageServiceImplTest {
         verify(recipeRepository).save(recipeArgumentCaptor.capture());
         Recipe savedRecipe = recipeArgumentCaptor.getValue();
         assertEquals(savedRecipe.getImage().length, multipartFile.getBytes().length);
+
+    }
+
+    @Test
+    public void renderImageFromDB() throws Exception {
+        //given
+        Long id = 1L;
+        String s = "fake image test";
+
+        RecipeCommand command = new RecipeCommand();
+        command.setId(id);
+        Byte[] bytes = new Byte[s.getBytes().length];
+
+        int i=0;
+
+        for (byte b: s.getBytes()) {
+            bytes[i++] = b;
+        }
+
+        command.setImage(bytes);
+
+        when(recipeService.findCommandById(id)).thenReturn(command);
+
+        //when
+        MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        byte[] responseBytes = response.getContentAsByteArray();
+
+        //then
+
+        assertEquals(s.getBytes().length, responseBytes.length);
 
     }
 
