a5fdc23f2a862bb9866a22a31297ab73f5f9a553
Bando85 <laczoandras@gmail.com>
2020-04-05 14:18:19 +0200

    save image to databse method added

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,5 +1,7 @@
 package guru.springframework.services;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,8 +14,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
-    public void saveImageFile(Long recipeId, MultipartFile File) {
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
         log.debug("SaveImageFile method invoked");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] bytes = new Byte[file.getBytes().length];
+
+            int i=0;
+
+            for (byte b: file.getBytes()) {
+                bytes[i++] = b;
+            }
+
+            recipe.setImage(bytes);
+
+            recipeRepository.save(recipe);
+
+        } catch (Exception e) {
+            log.error("Error in saveImageFile", e);
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,64 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Andras Laczo 2020. 04. 05.
+ */
+
+public class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain", "AndrasLaczo".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> recipeArgumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+
+        verify(recipeRepository).save(recipeArgumentCaptor.capture());
+        Recipe savedRecipe = recipeArgumentCaptor.getValue();
+        assertEquals(savedRecipe.getImage().length, multipartFile.getBytes().length);
+
+    }
+
+}
